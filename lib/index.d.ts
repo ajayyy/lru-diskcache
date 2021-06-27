2c1b728409771e06212637ced0317ad9d116ef31
Ajay Ramachandran <dev@ajay.app>
2021-06-26 22:20:34 -0400

    Add type def

---
+++ lib/index.d.ts
@@ -0,0 +1,30 @@
+import { Stream } from "stream";
+
+export interface CacheOptions {
+    max: number,
+    maxEntries: number | null,
+    maxAge: number | undefined,
+    onError: (err: Error) => void,
+}
+
+export default class DiskCache<K, V> {
+
+    constructor(rootPath: string, options: CacheOptions);
+
+    init(): void;
+
+    reset(): void;
+
+    has(key: string): boolean;
+
+    get(key: K, opts: {encoding?: string}): V;
+
+    // Returns size
+    set(key: K, dataOrSteam: V & Stream): Promise<number>;
+
+    del(key: K): void;
+
+    size(): number;
+
+    prune(): void;
+}
